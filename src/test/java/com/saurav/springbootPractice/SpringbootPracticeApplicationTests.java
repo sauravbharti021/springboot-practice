b1eb69f7c515a76ac976c97ecbb87d837747c012
Saurav Bharti <saurav.bharti@avisbudget.com>
2025-04-18 23:24:17 +0530

    initial commit for practice project

---
+++ src/test/java/com/saurav/springbootPractice/SpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurav.springbootPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
